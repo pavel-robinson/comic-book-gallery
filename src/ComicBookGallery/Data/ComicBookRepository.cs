fe914bd81546a5c23f20d974bae5cc8d3c3f7d2b
pavel-robinson <pclavelor74@yahoo.es>
2017-02-03 08:51:05 -0500

    Added comic Books list view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -56,6 +56,10 @@ namespace ComicBookGallery.Data
                 Favorite = false
             }
     };
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
